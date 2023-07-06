16880b534cca9d715978651e4eac65bc42e60509
Kay Cee <se.kaycee@gmail.com>
2023-07-07 00:56:09 +0100

    feat: add 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' Task 0: Add two floating-point numbers. '''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Add two floating-point numbers and returns the result.
+
+    Parameters:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Return:
+        float: The sum of a and b.
+    '''
+    return a + b
