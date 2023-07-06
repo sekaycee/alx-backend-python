d9d83794b7d19ae3ed36908cf9267a7529c2ec5c
Kay Cee <se.kaycee@gmail.com>
2023-07-07 00:57:46 +0100

    feat: add 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' Task 1: Write a type-annotated function concat '''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Concatenate two strings.
+
+    Args:
+    - str1 (str): The first string to concatenate
+    - str2 (str): The second string to concatenate
+
+    Return:
+    - str: The concatenated string
+    '''
+    return str1 + str2
