34eb699a8f063339c590a012511868b598cc13cf
Kay Cee <se.kaycee@gmail.com>
2023-07-07 01:42:44 +0100

    feat: add 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' Task 6: Write a type-annotated function sum_mixed_list '''
+
+from typing import List, Union
+from functools import reduce
+
+
+def sum_mixed_list(mixed_list: List[Union[int, float]]) -> float:
+    '''
+    Compute the sum of a list of integers and floating-point numbers.
+    '''
+    total = reduce(lambda a, b: a + b, mixed_list)
+    return total
