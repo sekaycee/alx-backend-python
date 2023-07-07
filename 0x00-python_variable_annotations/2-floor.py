e010fe06b375aac65e91dd3bd10bcadc00bd3308
Kay Cee <se.kaycee@gmail.com>
2023-07-07 01:24:36 +0100

    feat: add 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Task 2: Write a type-annotated function floor '''
+
+import math
+
+
+def floor(a: float) -> int:
+    ''' Compute the floor of a floating-point number. '''
+    return math.floor(a)
